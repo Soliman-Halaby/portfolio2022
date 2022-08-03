bc7718417ddb5fcb0a47cfd21eed835922b7155c
Soliman Halaby <soliman.al-halaby@hetic.net>
2022-08-03 22:13:05 +0200

    :star: Setup Components

---
+++ components/Hero/index.js
@@ -0,0 +1,13 @@
+import { HeroSection } from "./style";
+const Hero = () => {
+  return (
+    <HeroSection>
+      <p>Coucou</p>
+      <p>Coucou</p>
+      <p>Coucou</p>
+      <p>Coucou</p>
+    </HeroSection>
+  );
+};
+
+export default Hero;
