9a21975d28dd57bd90234df4fed2a42fe43aa7ec
Soliman Halaby <soliman.al-halaby@hetic.net>
2022-08-28 19:40:15 +0200

    Setup Test Page

---
+++ pages/contact/index.js
@@ -0,0 +1,18 @@
+import Layout from "../../layout/Default";
+
+import Head from "next/head";
+import Image from "next/image";
+// import styles from "../styles/Home.module.css";
+
+import Hero from "components/Hero";
+export default function About() {
+  return (
+    <Layout>
+      {/* <div className="coucou">
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum{" "}
+      </div> */}
+    </Layout>
+  );
+}
---
+++ pages/projet/index.js
@@ -0,0 +1,18 @@
+import Layout from "../../layout/Default";
+
+import Head from "next/head";
+import Image from "next/image";
+// import styles from "../styles/Home.module.css";
+
+import Hero from "components/Hero";
+export default function Projet() {
+  return (
+    <Layout reducedFooter>
+      {/* <div className="coucou">
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum{" "}
+      </div> */}
+    </Layout>
+  );
+}
