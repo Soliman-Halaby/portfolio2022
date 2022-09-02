9288bf44097601864e7caab56272d335e3b1398b
Soliman Halaby <soliman.al-halaby@hetic.net>
2022-09-02 14:36:21 +0200

    Fix build

--- pages/project/[title].js
+++ pages/project/[title].js
@@ -34,32 +34,30 @@ export default function Projet({}) {
   // Get projects - Next project section
   const allProjects = data.filter((project) => project.title !== title);
 
-  if (title) {
-    return (
-      <Layout reducedFooter>
-        <Hero title={projectData.title} tags={projectData.tags} />
-        <Description
-          title={projectData.content1.title}
-          label={projectData.content1.label}
-          subtitle={projectData.content1.subtitle}
-          text1={projectData.content1.text1}
-          text2={projectData.content1.text2}
-        />
-        <FullWidthImage src={projectData.images.fullwidth} />
-        <ImageTwoColumns
-          src={projectData.images.left}
-          src2={projectData.images.right}
-        />
-        <Description
-          title={projectData.content2.title}
-          label={projectData.content2.label}
-          subtitle={projectData.content2.subtitle}
-          text1={projectData.content2.text1}
-          text2={projectData.content2.text2}
-        />
-        <FullWidthImage src={projectData.images.fullwidth2} />
-        <BottomProject projects={allProjects} />
-      </Layout>
-    );
-  }
+  return (
+    <Layout reducedFooter>
+      <Hero title={projectData.title} tags={projectData.tags} />
+      <Description
+        title={projectData.content1.title}
+        label={projectData.content1.label}
+        subtitle={projectData.content1.subtitle}
+        text1={projectData.content1.text1}
+        text2={projectData.content1.text2}
+      />
+      <FullWidthImage src={projectData.images.fullwidth} />
+      <ImageTwoColumns
+        src={projectData.images.left}
+        src2={projectData.images.right}
+      />
+      <Description
+        title={projectData.content2.title}
+        label={projectData.content2.label}
+        subtitle={projectData.content2.subtitle}
+        text1={projectData.content2.text1}
+        text2={projectData.content2.text2}
+      />
+      <FullWidthImage src={projectData.images.fullwidth2} />
+      <BottomProject projects={allProjects} />
+    </Layout>
+  );
 }
