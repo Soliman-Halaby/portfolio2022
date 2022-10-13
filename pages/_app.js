47ce6e740b04494461b4326c889d44bf78f1fe7d
Soliman Halaby <soliman.al-halaby@hetic.net>
2022-10-13 22:19:12 +0200

    :rocket: Setup Locomotive Mobile

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,8 @@
 import { ThemeProvider, Global } from "@emotion/react";
-import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 
-import { useRef } from 'react'
-import { useRouter } from 'next/router'
-
+import { useRef } from "react";
+import { useRouter } from "next/router";
 
 import theme from "theme";
 import "styles/globals.css";
@@ -11,22 +10,26 @@ import "styles/globals.css";
 import globalStyles from "styles/global";
 
 export default function App({ Component, pageProps }) {
-
-  const { route } = useRouter()
-  const containerRef = useRef(null)
+  const { route } = useRouter();
+  const containerRef = useRef(null);
 
   return (
-
     <LocomotiveScrollProvider
-      options={{ smooth: true }}
+      options={{
+        smooth: true,
+        smartphone: { smooth: true },
+        reloadOnContextChange: true,
+      }}
       watch={[route]}
       containerRef={containerRef}
-      onUpdate={({ scroll }) => scroll.scrollTo(0, { duration: 0, disableLerp: true })}
+      onUpdate={({ scroll }) =>
+        scroll.scrollTo(0, { duration: 0, disableLerp: true })
+      }
       // onLocationChange={scroll => scroll.scrollTo(0, { duration: 0, disableLerp: true })} // If you want to reset the scroll position to 0 for example
     >
       <main data-scroll-container ref={containerRef}>
         <ThemeProvider theme={theme}>
-        <Global styles={globalStyles} />
+          <Global styles={globalStyles} />
           <Component {...pageProps} />
         </ThemeProvider>
       </main>
