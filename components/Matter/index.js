33d6ed09bdd9f547dddc03c311890528a0942917
Soliman Al Halaby <60599592+Soliman-Halaby@users.noreply.github.com>
2022-10-19 17:53:07 +0200

    :rocket: About page physics : setup mouse control

--- components/Matter/index.js
+++ components/Matter/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 
-import {Engine, Render, Composite, World, Bodies} from 'matter-js'
+import {Engine, Render, Composite, World, Bodies, Mouse, MouseConstraint, Runner} from 'matter-js'
 
 import { SceneContainer } from "./style";
 
@@ -10,7 +10,7 @@ const MatterComponent = ({}) => {
     const scene = useRef()
     const engine = useRef(Engine.create({}))
 
-    console.log(Composite)
+    console.log('mouse', Mouse, 'mouseConstraint', MouseConstraint)
     useEffect(() => {
         const clientWidth = document.body.clientWidth
         const clientHeight = document.body.clientHeight
@@ -26,39 +26,56 @@ const MatterComponent = ({}) => {
             }
         })
 
+
+        const wallTop = Bodies.rectangle(clientWidth / 2, 17, clientWidth, 30, { isStatic: true, 
+            render: {
+                fillStyle: 'red',
+                strokeStyle:'green',
+                lineWidth:3
+            } 
+        })
+
+        const wallLeft = Bodies.rectangle(17, clientHeight / 2, 30, clientHeight, { isStatic: true, 
+            render: {
+                fillStyle: 'red',
+                strokeStyle:'blue',
+                lineWidth:3
+            }
+        })
+
+        const ground = Bodies.rectangle(clientWidth / 2, clientHeight - 117, clientWidth, 30, { isStatic: true, 
+            render: {
+                fillStyle: 'red',
+                strokeStyle:'yellow',
+                lineWidth:3
+            }
+        })
+
+        const wallRight = Bodies.rectangle(clientWidth - 117, clientHeight / 2, 30, clientHeight, { isStatic: true, 
+            render: {
+                fillStyle: 'pink',
+                strokeStyle:'purple',
+                lineWidth:3,
+            },
+        })
+
+        // Add mouse control
+        let mouse = Mouse.create(render.canvas),
+        mouseConstraint = MouseConstraint.create(engine.current, {
+        mouse: mouse,
+        constraint: {
+            stiffness: 0.2,
+            render: {
+            visible: false
+            }
+        }
+        });
+
         // Gravity on World
-        engine.current.gravity.y = 3;
+        engine.current.gravity.y = 1;
 
         // Add rectangles surface
-        Composite.add(engine.current.world, [
-            Bodies.rectangle(clientWidth / 2, 17, clientWidth, 30, { isStatic: true, 
-                render: {
-                    fillStyle: 'red',
-                    strokeStyle:'green',
-                    lineWidth:3
-                } 
-            }),
-            Bodies.rectangle(17, clientHeight / 2, 30, clientHeight, { isStatic: true, 
-                render: {
-                    fillStyle: 'red',
-                    strokeStyle:'blue',
-                    lineWidth:3
-                }
-            }),
-            Bodies.rectangle(clientWidth / 2, clientHeight - 117, clientWidth, 30, { isStatic: true, 
-                render: {
-                    fillStyle: 'red',
-                    strokeStyle:'yellow',
-                    lineWidth:3
-                }
-            }),
-            Bodies.rectangle(clientWidth - 117, clientHeight / 2, 30, clientHeight, { isStatic: true, 
-                render: {
-                    fillStyle: 'pink',
-                    strokeStyle:'purple',
-                    lineWidth:3
-                }
-            }),
+        Composite.add(engine.current.world, [wallTop, wallLeft, ground, wallRight,
             // Rectangle elements
             Bodies.rectangle(
                 280,
@@ -69,8 +86,10 @@ const MatterComponent = ({}) => {
                     mass: 2,
                     restitution: 0.9,
                     friction: 0.1,
+                    frictionAir: 0.01,
                     render:{
-                        fillStyle:'lightblue'
+                        fillStyle:'transparent',
+                        strokeStyle:'black',
                     }
                 }
             ),
@@ -80,30 +99,34 @@ const MatterComponent = ({}) => {
                 290,
                 100,
                 {
-                    mass: 2,
-                    restitution: 0.9,
+                    mass: 10,
+                    restitution: 0.1,
                     friction: 0.1,
-                    frictionAir: 0.1,
+                    frictionAir: 0.02,
                     render:{
-                        fillStyle:'red'
+                        strokeStyle: '#4a485b',
+                        fillStyle:'white'
                     }
                 }
             )
           ])
           
+
+          World.add(engine.current.world, mouseConstraint)
           // run the engine
-        Engine.run(engine.current)
+        Runner.run(engine.current)
         Render.run(render)
+        render.mouse = mouse
    
 
         return () => {
             Render.stop(render)
             Composite.clear(engine.current.world)
             Engine.clear(engine.current)
-            
             render.canvas.remove()
             render.canvas = null
             render.context = null
+            render.mouse = null
             render.textures = {}
         }
     }, [])
