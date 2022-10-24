846e88d5e60f2493d301e370f9868b64f58c7cbe
Soliman Halaby <soliman.al-halaby@hetic.net>
2022-10-24 16:48:00 +0200

    :rocket: matter: create elements in dom

--- components/Matter/index.js
+++ components/Matter/index.js
@@ -1,149 +1,380 @@
-import React, { useState, useRef, useEffect } from "react";
+// import React, { useState, useRef, useEffect } from "react";
 
-import {Engine, Render, Composite, World, Bodies, Mouse, MouseConstraint, Runner} from 'matter-js'
+// import {
+//   Engine,
+//   Render,
+//   Composite,
+//   World,
+//   Bodies,
+//   Mouse,
+//   MouseConstraint,
+//   Runner,
+// } from "matter-js";
 
-import { SceneContainer, Container, Title} from "./style";
+// import { SceneContainer, Container, Title } from "./style";
 
+// const MatterComponent = ({}) => {
+//   const scene = useRef();
+//   const engine = useRef(Engine.create({}));
+
+//   // Variables
+
+//   const GRAVITY = 1;
+//   const WIREFRAMES = false;
+
+//   const COLOR = {
+//     BACKGROUND: "transparent",
+//     GROUND: "#1E1E1E",
+//   };
+
+//   // Scene walls
+//   const wall = (x, y, width, height, stroke, fill) => {
+//     return Bodies.rectangle(x, y, width, height, {
+//       render: {
+//strokeStyle: stroke,
+//         fillStyle: fill,
+//         lineWidth: 1,
+//       },
+//       isStatic: true,
+//     });
+//   };
+
+//   // Function to generate rectangles in scene
+//   const rect = (x, y, width, height, angle, color) => {
+//     return Bodies.rectangle(x, y, width, height, {
+//       angle: angle,
+//       render: {
+//         fillStyle: color,
+//       },
+//       mass: 7,
+//       restitution: 0.3,
+//       friction: 0.2,
+//       frictionAir: 0.01,
+//     });
+//   };
+
+//   useEffect(() => {
+//     let clientWidth = document.body.clientWidth;
+//     let clientHeight = document.body.clientHeight;
+
+//     const render = Render.create({
+//       element: scene.current,
+//       engine: engine.current,
+//       options: {
+//         width: clientWidth,
+//         height: clientHeight,
+//         wireframes: WIREFRAMES,
+//         background: "transparent",
+//       },
+//     });
+
+//     // const wallTop = wall(
+//     //   clientWidth / 2,
+//     //   17,
+//     //   clientWidth,
+//     //   150,
+//     //   "red",
+//     //   "transparent"
+//     // );
+
+//     // Scene walls
+//     const wallLeft = wall(
+//       -100,
+//       clientHeight / 2,
+//       200,
+//       clientHeight * 2,
+//       "blue",
+//       "red"
+//     );
+//     const wallRight = wall(
+//       clientWidth + 100,
+//       clientHeight / 2,
+//       200,
+//       clientHeight * 2,
+//       "purple",
+//       "pink"
+//     );
+//     const ground = wall(
+//       clientWidth / 2,
+//       clientHeight + 50,
+//       clientWidth,
+//       250,
+//       COLOR.GROUND,
+//       COLOR.BACKGROUND
+//     );
+
+//     // Add mouse control
+//     let mouse = Mouse.create(render.canvas),
+//       mouseConstraint = MouseConstraint.create(engine.current, {
+//         mouse: mouse,
+//         constraint: {
+//           stiffness: 0.2,
+//           render: {
+//             visible: false,
+//           },
+//         },
+//       });
+
+//     // Data structure to store all rectangles
+//     const datas = ["Paris", "test", "tigran"];
+
+//     // Create rectangles with random position and size (depending on the data)
+//     const generateBlock = () => {
+//       return Composite.add(
+//         engine.current.world,
+//         rect(
+//           Math.random() * clientWidth,
+//           Math.random() * -clientHeight,
+//           290,
+//           100,
+//           Math.random() * 180,
+//           "white"
+//         )
+//       );
+//     };
+
+//     // Add all generated rectangles to the scene
+//     datas.map((data, i) => {
+//       generateBlock();
+//     });
+
+//     // Gravity on World
+//     engine.current.gravity.y = GRAVITY;
+
+//     // Add rectangles surface
+//     Composite.add(engine.current.world, [
+//       // wallTop,
+//       wallLeft,
+//       ground,
+//       wallRight,
+//     ]);
+
+//     // Add mouse mouse control to the scene
+//     Composite.add(engine.current.world, mouseConstraint);
+
+//     // Run the engine
+//     Runner.run(engine.current);
+//     Render.run(render);
+
+//     render.mouse = mouse;
+//     render.options.showPerformance = true;
+//     // if (typeof window !== "undefined" && render.canvas && render.options) {
+//     //   console.log(render.canvas);
+//     //   window.addEventListener("resize", () => {
+//     //     clientWidth = document.documentElement.clientWidth;
+
+//     //     clientHeight = document.documentElement.clientHeight;
+//     //     // render.bounds.max.x = window.innerWidth;
+//     //     // render.bounds.max.y = window.innerHeight;
+//     //     // render.options.width = window.innerWidth;
+//     //     // render.options.height = window.innerHeight;
+//     //     // render.canvas.width = window.innerWidth;
+//     //     // render.canvas.height = window.innerHeight;
+//     //   });
+//     // }
+
+//     return () => {
+//       Render.stop(render);
+//       Composite.clear(engine.current.world);
+//       Engine.clear(engine.current);
+//       render.canvas.remove();
+//       render.canvas = null;
+//       render.context = null;
+//       render.mouse = null;
+//     };
+//   }, []);
+
+//   return (
+//     <Container>
+//       <Title>
+//         Get to know
+//         <br />
+//         me better
+//       </Title>
+//       <SceneContainer ref={scene} />
+//     </Container>
+//   );
+// };
+
+// export default MatterComponent;
+import React, { useState, useRef, useEffect, Fragment } from "react";
+
+import {
+  Engine,
+  Render,
+  Composite,
+  World,
+  Bodies,
+  Mouse,
+  MouseConstraint,
+  Runner,
+} from "matter-js";
+
+import {
+  SceneContainer,
+  Container,
+  Title,
+  MatterContainer,
+  Box,
+  Ground,
+} from "./style";
 
 const MatterComponent = ({}) => {
+  const requestRef = useRef();
+  const boxRef = useRef([]);
 
-    const scene = useRef()
-    const engine = useRef(Engine.create({}))
+  //   // Variables
 
+  const GRAVITY = 1;
+  const WIREFRAMES = false;
 
-    // Variables
+  const COLOR = {
+    BACKGROUND: "transparent",
+    GROUND: "#1E1E1E",
+  };
 
-    const GRAVITY = 1;
-    const WIREFRAMES = false;
+  // Scene walls
+  const wall = (x, y, width, height, stroke, fill) => {
+    return Bodies.rectangle(x, y, width, height, {
+      render: {
+        strokeStyle: stroke,
+        fillStyle: fill,
+        lineWidth: 1,
+      },
+      isStatic: true,
+    });
+  };
 
-    const COLOR = {
-        BACKGROUND: 'transparent',
-        GROUND: '#1E1E1E'
-    }
+  // Function to generate blocks in scene
+  const rect = (x, y, width, height, angle, color) => {
+    return Bodies.rectangle(x, y, width, height, {
+      angle: angle,
+      render: {
+        fillStyle: color,
+      },
+      mass: 7,
+      restitution: 0.3,
+      friction: 0.2,
+      frictionAir: 0.01,
+    });
+  };
+
+  //     // Data structure to store all rectangles
+
+  useEffect(() => {
+    // animate();
+
+    const engine = Engine.create();
+
+    let clientWidth = document.body.clientWidth;
+    let clientHeight = document.body.clientHeight;
+    const box = {
+      body: rect(
+        Math.random() * clientWidth,
+        Math.random() * -clientHeight,
+        250,
+        75,
+        Math.random() * 180,
+        "white"
+      ),
+
+      elem: boxRef.current[0],
+
+      render() {
+        console.log(boxRef);
+        // console.log(this.body);
+        this.elem.textContent = "Ultranoir";
+        const { x, y } = this.body.position;
+        this.elem.style.top = `${y - 20}px`;
+        this.elem.style.left = `${x - 20}px`;
+        this.elem.style.transform = `rotate(${this.body.angle}rad)`;
+      },
+    };
+
+    //     // Create rectangles with random position and size (depending on the data)
+    const generateBlock = () => {
+      return Composite.add(
+        engine.current.world,
+        rect(
+          Math.random() * clientWidth,
+          Math.random() * -clientHeight,
+          290,
+          100,
+          Math.random() * 180,
+          "white"
+        )
+      );
+    };
 
     // Scene walls
-    const wall = (x,y, width, height, stroke, fill) => {
-        return Bodies.rectangle(x, y, width, height, {
-            render:{
-                strokeStyle: stroke,
-                fillStyle: fill,
-                lineWidth: 1
-            },
-            isStatic: true,
-        })
-    }
 
-    // Function to generate rectangles in scene
-    const rect = (x, y, width, height, angle, color) => {
-        return Bodies.rectangle(x, y, width, height, {
-            restitution: 0.3,
-            angle: angle,
-            render:{
-                fillStyle: color
-            },
-            mass: 10,
-            // inverseMass: 1/10,
-            // density: 0.01,
-            // restitution: 0.9,
-            // friction: 0.1,
-            // frictionAir: 0.01
-        })
-    }
+    const ground = wall(
+      clientWidth / 2,
+      clientHeight + 50,
+      clientWidth,
+      250,
+      "pink",
+      "pink"
+    );
 
-    useEffect(() => {
-        let clientWidth = document.body.clientWidth
-        let clientHeight = document.body.clientHeight
-        
-        const render = Render.create({
-            element: scene.current,
-            engine: engine.current,
-            options:{
-                width: clientWidth ,
-                height: clientHeight,
-                wireframes: WIREFRAMES,
-                background: 'transparent',
-            }
-        })
+    const wallLeft = wall(
+      -100,
+      clientHeight / 2,
+      200,
+      clientHeight * 2,
+      "blue",
+      "red"
+    );
+    const wallRight = wall(
+      clientWidth + 100,
+      clientHeight / 2,
+      200,
+      clientHeight * 2,
+      "purple",
+      "pink"
+    );
 
-        const wallTop = wall(clientWidth / 2, 17, clientWidth, 150, 'red', 'blue')
-        const wallLeft = wall(-100, clientHeight / 2, 200, clientHeight, 'blue', 'red')
-        const wallRight = wall(clientWidth + 100, clientHeight / 2, 200, clientHeight, 'purple', 'pink')
-        const ground = wall(clientWidth / 2, clientHeight + 50, clientWidth, 250, COLOR.GROUND, COLOR.BACKGROUND)
+    // Controls box with mouse
+    const mouseConstraint = MouseConstraint.create(engine, {
+      element: document.body,
+    });
 
-        // Add mouse control
-        let mouse = Mouse.create(render.canvas),
-        mouseConstraint = MouseConstraint.create(engine.current, {
-        mouse: mouse,
-        constraint: {
-            stiffness: 0.2,
-            render: {
-            visible: false
-            }
-        }
-        });
+    // Add elements in the world
+    Composite.add(engine.world, [
+      box.body,
+      ground,
+      wallLeft,
+      wallRight,
+      mouseConstraint,
+    ]);
 
+    // Re render elements to get box position
+    const rerender = () => {
+      box.render();
+      Engine.update(engine);
+      requestRef.current = requestAnimationFrame(rerender);
+    };
+    rerender();
+    return () => cancelAnimationFrame(requestRef.current);
+  }, []);
 
-        // Datas to generate block
-        const datas = [
-            'test', 'blabla'
-        ]
+  const datas = ["Paris", "test", "tigran"];
 
-        console.log(datas.length)
-        // Gravity on World
-        engine.current.gravity.y = GRAVITY;
+  return (
+    <MatterContainer>
+      <Fragment>
+        {datas.map((data, i) => {
+          return <Box key={i} ref={(el) => (boxRef.current[i] = el)} />;
+        })}
+        <Ground />
+      </Fragment>
+      <Title>
+        Get to know
+        <br />
+        me better
+      </Title>
+    </MatterContainer>
+  );
+};
 
-        // Add rectangles surface
-        Composite.add(engine.current.world, [wallTop, wallLeft, ground, wallRight,
-            // Rectangle elements
-
-            // rect(280, 60, 100, 20, 30, 'red'),
-            rect(290, 150, 290, 100, 100, 'white'),
-        
-          ])
-          
-
-          World.add(engine.current.world, mouseConstraint)
-          // run the engine
-        Runner.run(engine.current)
-        Render.run(render)
-        render.mouse = mouse
-   
-
-        if(typeof window !== 'undefined' && render.canvas && render.options){
-            console.log(render.canvas)
-            window.addEventListener('resize', () => { 
-                clientWidth = document.documentElement.clientWidth
-                
-                clientHeight = document.documentElement.clientHeight
-                // render.bounds.max.x = window.innerWidth;
-                // render.bounds.max.y = window.innerHeight;
-                // render.options.width = window.innerWidth;
-                // render.options.height = window.innerHeight;
-                // render.canvas.width = window.innerWidth;
-                // render.canvas.height = window.innerHeight;
-              });
-        }
-
-        return () => {
-            Render.stop(render)
-            Composite.clear(engine.current.world)
-            Engine.clear(engine.current)
-            render.canvas.remove()
-            render.canvas = null
-            render.context = null
-            render.mouse = null
-            render.textures = {}
-        }
-
-    }, [])
-
-
-    return(
-        <Container>
-            {/* <Title>Get to know<br/>me better</Title> */}
-            <SceneContainer ref={scene}/>
-        </Container>
-    )
-}
-
-export default MatterComponent
+export default MatterComponent;
--- components/Matter/style.js
+++ components/Matter/style.js
@@ -1,33 +1,50 @@
 import styled from "@emotion/styled";
 
-export const SceneContainer = styled.div`
-    width: 100%;
-    margin: 0 auto;
-    height: 100vh;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    canvas{
-        background: transparent !important;
-        z-index: 999;
-    }
-`
-
 export const Container = styled.div`
-    width: 100%;
-    height: 100%;
-    position: relative;
-`
+  width: 100%;
+  user-select: none;
+  height: 100%;
+  position: relative;
+`;
 
+export const Box = styled.div`
+  position: absolute;
+  /* background: #111; */
+  /* height: 40px;
+      width: 40px; */
+  user-select: none;
+  cursor: pointer;
+  background: ${({ theme }) => theme.colors.grey2};
+  text-transform: uppercase;
+  z-index: 2;
+  border-radius: 8px;
+  border: 1px solid ${({ theme }) => theme.colors.grey1};
+  padding: 5px 10px;
+  font-size: 40px;
+  font-family: ${({ theme }) => theme.fonts.sansSerif};
+`;
+
+export const Ground = styled.div`
+  position: absolute;
+  bottom: 74px;
+  height: 1px;
+  width: 100%;
+  left: 0;
+  background: #666;
+`;
 export const Title = styled.h2`
-    position: absolute;
-    width: 100%;
-    text-align: center;
-    font-family: ${({theme}) => theme.fonts.serif};
-    font-weight: 400;
-    font-size: 200px;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-`
+  position: absolute;
+  width: 100%;
+  text-align: center;
+  font-family: ${({ theme }) => theme.fonts.serif};
+  font-weight: 400;
+  user-select: none;
+  font-size: 200px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const MatterContainer = styled.div`
+  z-index: 9999;
+`;
