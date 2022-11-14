934a8ba638759893949fcf172800bfd54e686d50
Soliman Halaby <soliman.al-halaby@hetic.net>
2022-11-14 20:49:30 +0100

    :lipstick: Box styling

--- components/Matter/style.js
+++ components/Matter/style.js
@@ -13,16 +13,17 @@ export const Box = styled.div`
   background: ${({ theme }) => theme.colors.grey2};
   text-transform: uppercase;
   z-index: 2;
-  font-weight: 400;
+  font-family: ${({ theme }) => theme.fonts.sansSerif};
+  font-weight: 300;
   border-radius: 8px;
   letter-spacing: 0px;
   border: 1px solid ${({ theme }) => theme.colors.grey1};
   padding: 5px 10px;
-  font-size: 40px;
-  font-family: ${({ theme }) => theme.fonts.sansSerif};
+  font-size: 4rem;
 
+  /* font-size: 40px; */
   &.rounded {
-    /* border-radius: 200px; */
+    border-radius: 200px;
     padding: 5px 20px;
   }
 
@@ -35,7 +36,7 @@ export const SceneContainer = styled.div`
   width: 100vw;
   /* overflow: hidden; */
   canvas {
-    /* opacity: 0; */
+    opacity: 0;
   }
 `;
 export const Ground = styled.div`
@@ -54,13 +55,18 @@ export const Title = styled.h2`
   font-family: ${({ theme }) => theme.fonts.serif};
   font-weight: 400;
   user-select: none;
-  font-size: 10vw;
+  font-size: 20rem;
+  line-height: 19rem;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
 
+  ${maxMedia.small} {
+    font-size: 10rem;
+    line-height: 10rem;
+  }
   ${maxMedia.xs} {
-    font-size: 20vw;
+    /* font-size: 20vw; */
   }
 `;
 
@@ -70,6 +76,7 @@ export const Detail = styled.div`
   pointer-events: none;
   right: 0;
   user-select: none;
+  display: none;
   width: 50vw;
   height: 60vh;
   z-index: 9;
@@ -78,7 +85,7 @@ export const Detail = styled.div`
   transform: translateY(100%);
   visibility: hidden;
   /* display: none; */
-  opacity: 0;
+  /* opacity: 0; */
   background: ${({ theme }) => theme.colors.lime};
   will-change: transform;
   transition: all 0.5s ease-in-out;
@@ -88,7 +95,7 @@ export const Detail = styled.div`
     visibility: visible;
     transform: translateY(0%);
     /* display: block; */
-    opacity: 1;
+    /* opacity: 1; */
   }
 
   ${maxMedia.small} {
