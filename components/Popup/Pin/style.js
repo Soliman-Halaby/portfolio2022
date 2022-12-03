a585ef91fbe964f6553e172904eb139cd207fcfb
Soliman Halaby <soliman.al-halaby@hetic.net>
2022-12-03 21:41:26 +0100

    :rocket: Pin : Add keyframe animation

--- components/Popup/Pin/style.js
+++ components/Popup/Pin/style.js
@@ -4,48 +4,48 @@ import { css } from "@emotion/react";
 import { maxMedia, title } from "styles/mixins";
 
 export const SuccessMessageContent = styled.p`
+  @keyframes up {
+    0% {
+      transform: translateY(0);
+    }
+    4% {
+      transform: translateY(-100%);
+    }
+    100% {
+      transform: translateY(-100%);
+    }
+  }
   /* padding: 7.5px; */
+  animation-name: up;
+  animation-duration: 7s;
+  animation-timing-function: ease;
+  animation-delay: 4s;
+  animation-iteration-count: infinite;
+  animation-direction: normal;
+  animation-fill-mode: none;
+  animation-play-state: running;
+  animation-timeline: auto;
 `;
 
 export const SuccessMessageContentContainer = styled.div`
   overflow: hidden;
 
   display: flex;
+
   &:nth-of-type(1) {
     ${SuccessMessageContent} {
       will-change: transform;
       transform-style: preserve-3d;
       transition: 0.2s;
-      /* transition-delay: calc(0.05s * var(--index)); */
-      transform-origin: bottom;
-      display: inline-block;
     }
   }
   &:nth-of-type(2) {
     ${SuccessMessageContent} {
-      /* @keyframes animEnter {
-        0% {
-          transform: translateY(100%);
-        }
-        // 50% {
-        //     transform: rotate(360deg);
-        // }
-        100% {
-          transform: translateY(0%);
-          // opacity: 0;
-        }
-      } */
        {
         position: absolute;
         will-change: transform;
-        /* transform-style: preserve-3d; */
         transition: 0.3s;
-        /* transition-delay: calc(0.05s * var(--index)); */
-        /* transform-origin: top; */
         display: inline-block;
-        animation: animEnter 0.3s forwards;
-        animation-delay: 10s;
-        /* transform: translate3d(0, 100%, 0) rotateX(-90deg); */
         transform: translateY(100%);
       }
     }
@@ -54,13 +54,12 @@ export const SuccessMessageContentContainer = styled.div`
 export const SuccessMessage = styled.div`
   opacity: ${({ colored }) => (colored ? 1 : 0)};
   position: absolute;
-  overflow: ${({ colored }) => (colored ? "visible" : "hidden")};
+  overflow: hidden;
   padding: 7.5px;
   top: ${({ top }) => `${top}rem`};
   left: ${({ left }) => `${left}rem`};
   transform: translate(-50%, -50%);
   transform: rotate(-4.6deg);
-  /* padding: 7.5px; */
   background: ${({ colored, theme }) =>
     colored ? `${theme.colors.lime}` : `${theme.colors.grey2}`};
   border-radius: 39px;
@@ -69,26 +68,11 @@ export const SuccessMessage = styled.div`
   font-weight: 300;
   text-transform: uppercase;
   font-size: ${({ colored }) => (colored ? `1rem` : `1.25rem`)};
-  /* border-style: dashed; */
   line-height: initial;
   display: flex;
   flex-direction: column;
 
   ${maxMedia.small} {
     font-size: ${({ colored }) => (colored ? `1.2` : `1.25rem`)};
-  }
-
-  &:hover {
-    ${SuccessMessageContent} {
-      &:nth-of-type(1) {
-        transform: translateY(-100%);
-        /* transform: translate3d(0, -100%, 0) rotateX(-90deg); */
-      }
-
-      &:nth-of-type(2) {
-        /* transform: translate3d(0, 0%, 0) rotateX(0deg); */
-        transform: translateY(0%);
-      }
-    }
   }
 `;
