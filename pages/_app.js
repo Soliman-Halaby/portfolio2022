5f51704405a969e1816c8bfa1ea78bde78e20a24
Soliman Halaby <soliman.al-halaby@hetic.net>
2022-12-06 19:25:48 +0100

    :fire: Global : clean code

--- pages/_app.js
+++ pages/_app.js
@@ -10,31 +10,12 @@ import "styles/globals.css";
 
 import globalStyles from "styles/global";
 export default function App({ Component, pageProps }) {
-  const { route } = useRouter();
-  const containerRef = useRef(null);
-
   return (
-    // <LocomotiveScrollProvider
-    //   options={{
-    //     smooth: true,
-    //     smartphone: { smooth: true },
-    //     tablet: { smooth: true },
-    //     reloadOnContextChange: true,
-    //   }}
-    //   watch={[route]}
-    //   containerRef={containerRef}
-    //   onUpdate={({ scroll }) =>
-    //     scroll.scrollTo(0, { duration: 0, disableLerp: true })
-    //   }
-    // >
-    //   <main data-scroll-container ref={containerRef}>
     <RecoilRoot>
       <ThemeProvider theme={theme}>
         <Global styles={globalStyles} />
         <Component {...pageProps} />
       </ThemeProvider>
     </RecoilRoot>
-    //   </main>
-    // </LocomotiveScrollProvider>
   );
 }
