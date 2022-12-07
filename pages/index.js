1fe8cf947b63ab8c28b8bdf5da8efa6295e75059
Soliman Halaby <soliman.al-halaby@hetic.net>
2022-12-07 23:11:52 +0100

    :iphone: Project : update content display

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,14 @@ import { useState, useRef, useEffect } from "react";
 
 import image from "../public/index-hero.png";
 
+import { useIsMobile } from "hook";
 import CustomCursor from "@/components/Cursor";
 import Hero from "@/components/HeroHome";
 import ProjectWork from "@/components/Work";
 
 export default function Home() {
+  const isMobile = useIsMobile();
+
   return (
     <Layout
       title="Soliman Al Halaby - Home"
@@ -23,7 +26,11 @@ export default function Home() {
         <Hero title={`Soliman Al Halaby, \ncreative developer`} subtitle={`Looking for an \ninternship`} sectionTitle={`Based in paris, \nfrom cairo`} image="/home-hero-image.png" label="Contact me" />
         )} */}
       <Hero
-        title={`Soliman Al Halaby,<br/>creative developer`}
+        title={
+          isMobile
+            ? `Soliman Al<br/>Halaby,creative<br/>developer`
+            : `Soliman Al Halaby,<br/>creative developer`
+        }
         subtitle={`Looking for an <br/>internship`}
         sectionTitle={`Based in paris, <br/>from cairo`}
         image={image}
