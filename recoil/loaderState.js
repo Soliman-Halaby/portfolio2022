207e67abc45915f79f6244d4e918c72385034d04
Soliman Halaby <soliman.al-halaby@hetic.net>
2022-12-12 22:25:04 +0100

    :tada: Loader : update handle exit animation

--- recoil/loaderState.js
+++ recoil/loaderState.js
@@ -4,8 +4,13 @@
 import { atom } from "recoil";
 
 export const LOADER_KEY = "LoaderManager";
-
+export const LOADER_ANIM = "LoaderAnim";
 export const loaderState = atom({
   key: LOADER_KEY,
+  default: true,
+});
+
+export const loaderAnim = atom({
+  key: LOADER_ANIM,
   default: false,
 });
