655165ff3ffabb0042129257137ec99e2a583096
Soliman Halaby <soliman.al-halaby@hetic.net>
2022-12-24 19:20:04 +0200

    :tada: App : Add useEffect for my amazing log

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { ThemeProvider, Global } from "@emotion/react";
 import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 
 import { RecoilRoot } from "recoil";
@@ -10,7 +10,9 @@ import "styles/globals.css";
 
 import globalStyles from "styles/global";
 export default function App({ Component, pageProps }) {
-  console.log("what are you looking for ? ( ͡◉ ͜ʖ ͡◉)");
+  useEffect(() => {
+    console.log("what are you looking for ? ( ͡◉ ͜ʖ ͡◉)");
+  }, []);
   return (
     <RecoilRoot>
       <ThemeProvider theme={theme}>
